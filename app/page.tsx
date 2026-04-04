afc1e230eeab0ecf88b62a55d616b1ea37de5569
Caleb H - Student <masterfrederick7410@gmail.com>
2026-04-04 15:27:45 -0400

    this

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,5 @@
 //this is the main page of the app, it will be rendered when the user visits the root url
+import React from 'react';  
+import { Card, CardContent, CardHeader, CardDescription} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
