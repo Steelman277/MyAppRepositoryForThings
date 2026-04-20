5cc7210a2abb9b382a1f07ed14da5ccc9ee79207
Caleb H - Student <masterfrederick7410@gmail.com>
2026-04-20 18:43:19 -0400

    this

--- app/file.lua
+++
@@ -1,9 +0,0 @@
-thunk = balatro
-local thunk
-function thunk.load(filename)
-  local file = love.filesystem.newFile(filename)
-  file:open("r")
-  local data = file:read()
-  file:close()
-  return data
-end
